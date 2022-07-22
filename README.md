fe963e38e5892bd2b1541c3322e8c50debebb56e
Sedoc-Eenaj <107987860+Sedoc-Eenaj@users.noreply.github.com>
2022-07-22 15:19:23 +1000

    add files to repo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FinalSampleProject
+
+1. npm init
+2. add prompt-sync with npm `install prompt-sync`
---
+++ student.js
@@ -0,0 +1,5 @@
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let stuName = prompt("Enter student name: ")
+console.log("Student name : " + stuName);
